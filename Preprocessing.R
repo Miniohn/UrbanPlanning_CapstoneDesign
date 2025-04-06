9ecde9851882cdd365af960855a8b2717975ed76
miniohn <22100456@handong.ac.kr>
2025-04-06 17:25:07 +0900

    2015 data add

--- Preprocessing.R
+++ Preprocessing.R
@@ -9,27 +9,33 @@ library(tidygraph)
 library(stringr)
 library(tidyr)
 library(ggplot2)
+library(tibble)
 
 
 ## load data
 
 # analyzing data
-data_2010 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/2010_industry_data.xlsx",sheet="industry_2010") %>%
+data_2010 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2010_industry_data.xlsx",sheet="industry_2010") %>%
   data.frame()
-data_2020 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/2020_industry_data.xlsx",sheet="industry_2020") %>%
+data_2015 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2015_industry_data.xlsx",sheet="industry_2015") %>%
   data.frame()
-data_2022 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/2022_industry_data.xlsx",sheet="industry_2022") %>%
+data_2020 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2020_industry_data.xlsx",sheet="industry_2020") %>%
+  data.frame()
+data_2022 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2022_industry_data.xlsx",sheet="industry_2022") %>%
   data.frame()
 
 # code data
-code_2010 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/2010_industry_data.xlsx",sheet="code_2010") %>% 
+code_2010 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2010_industry_data.xlsx",sheet="code_2010") %>% 
   data.frame()
-code_2020 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/2020_industry_data.xlsx",sheet="code_industry_2020") %>% 
+code_2015 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2015_industry_data.xlsx",sheet="code_2015") %>% 
   data.frame()
-code_2022 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/2022_industry_data.xlsx",sheet="code_industry_2022") %>% 
+code_2020 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2020_industry_data.xlsx",sheet="code_2020") %>% 
+  data.frame()
+code_2022 <- read_xlsx("/Users/haley/Desktop/2025-1/Research/data/Korea/data_analyze/2022_industry_data.xlsx",sheet="code_2022") %>% 
   data.frame()
 
 head(code_2010)
+head(code_2015)
 head(code_2020)
 head(code_2022)
 
@@ -41,13 +47,18 @@ head(code_2022)
 
 # new variables 1
 data_2010_n <- data_2010
+data_2015_n <- data_2015
 data_2020_n <- data_2020
 data_2022_n <- data_2022
 
 # colnames <- rownames
 data_2010_n <- data_2010_n %>% select(-...1)
-row.names(data_2010_n)<-colnames(data_2010_n)
+row.names(data_2010_n)<- colnames(data_2010_n)
 head(data_2010_n)
+
+data_2015_n <- data_2015_n %>% select(-...1)
+row.names(data_2015_n)<- colnames(data_2015_n)
+head(data_2015_n)
 
 data_2020_n <- data_2020_n %>% select(-...1)
 row.names(data_2020_n)<-colnames(data_2020_n)
@@ -70,6 +81,27 @@ data_2010_table <- data_2010_n %>%
 data_2010_table <- data_2010_table %>% data.frame()
 head(data_2010_table,15)
 data_2010_table %>% str()
+
+data_2015_table <- data_2015_n %>%
+  as.data.frame() %>%
+  mutate(from = rownames(.)) %>%
+  pivot_longer(
+    cols = -from,
+    names_to = "to",
+    values_to = "weight"
+  )
+data_2015_table <- data_2015_table %>% data.frame()
+head(data_2015_table,15)
+data_2015_table %>% str()
+
+# 1. wait! : names preprocessing
+# 2. groupby -> from, to
+data_2015_table<-
+  data_2015_table %>%
+  mutate(from_new = substr(from,1,4),
+       to_new = substr(to,1,4)) %>% select(from_new,to_new,weight) %>% rename(from=from_new, to=to_new) %>%
+  group_by(from, to) %>% summarise(weight=sum(weight)) %>% as.data.frame()
+
 
 data_2020_table <- data_2020_n %>%
   as.data.frame() %>%
@@ -95,9 +127,11 @@ head(data_2022_table,15)
 
 # from!=to | weight != 0 
 data_2010_fin <-data_2010_table %>% filter(from!=to) %>% filter(weight!=0)
+data_2015_fin <-data_2015_table %>% filter(from!=to) %>% filter(weight!=0)
 data_2020_fin <-data_2020_table %>% filter(from!=to) %>% filter(weight!=0)
 data_2022_fin <-data_2022_table %>% filter(from!=to) %>% filter(weight!=0)
 
 head(data_2010_fin,15)
+head(data_2015_fin,15)
 head(data_2020_fin,15)
 head(data_2022_fin,15)
--- SNA.R
+++ SNA.R
@@ -9,16 +9,19 @@ library(openxlsx)
 
 ## Data
 head(data_2010_fin)
+head(data_2015_fin)
 head(data_2020_fin)
 head(data_2022_fin)
 
 # use "igraph_from_data_frame" function
 g_2010 <- graph_from_data_frame(data_2010_fin, directed = TRUE)
+g_2015 <- graph_from_data_frame(data_2015_fin, directed = TRUE)
 g_2020 <- graph_from_data_frame(data_2020_fin, directed = TRUE)
 g_2022 <- graph_from_data_frame(data_2022_fin, directed = TRUE)
 
 # calculate "eigenvector"
 ev_2010 <- eigen_centrality(g_2010, directed = TRUE, weights = E(g_2010)$value)
+ev_2015 <- eigen_centrality(g_2015, directed = TRUE, weights = E(g_2015)$value)
 ev_2020 <- eigen_centrality(g_2020, directed = TRUE, weights = E(g_2020)$value)
 ev_2022 <- eigen_centrality(g_2022, directed = TRUE, weights = E(g_2022)$value)
 
@@ -33,6 +36,15 @@ df_ec_2010 <-
 
 df_ec_2010$eigen_centrality <- round(df_ec_2010$eigen_centrality, 6)
 df_ec_2010 <- df_ec_2010 %>% arrange(desc(eigen_centrality))
+
+# 2015
+df_ec_2015 <-
+  ev_2015$vector %>% as.data.frame() %>%
+  tibble::rownames_to_column("id") %>%
+  rename(eigen_centrality = ".")
+
+df_ec_2015$eigen_centrality <- round(df_ec_2015$eigen_centrality, 6)
+df_ec_2015 <- df_ec_2015 %>% arrange(desc(eigen_centrality))
 
 # 2020
 df_ec_2020 <-
@@ -53,6 +65,7 @@ df_ec_2022$eigen_centrality <- round(df_ec_2022$eigen_centrality, 6)
 df_ec_2022 <- df_ec_2022 %>% arrange(desc(eigen_centrality))
 
 head(df_ec_2010)
+head(df_ec_2015)
 head(df_ec_2020)
 head(df_ec_2022)
 
@@ -61,20 +74,24 @@ head(df_ec_2022)
 
 # preprocessing code name
 code_2010$code<-paste0("X",code_2010$code)
+code_2015$code<-paste0("X",code_2015$code)
 code_2020$code<-paste0("X",code_2020$code)
 code_2022$code<-paste0("X",code_2022$code)
 
 head(code_2010)
+head(code_2015)
 head(code_2020)
 head(code_2022)
 
 
 # merge
 final_2010 <- left_join(df_ec_2010,code_2010,by = c("id" = "code")) %>% select(id,title,eigen_centrality)
+final_2015 <- left_join(df_ec_2015,code_2015,by = c("id" = "code")) %>% select(id,title,eigen_centrality)
 final_2020 <- left_join(df_ec_2020,code_2020,by = c("id" = "code")) %>% select(id,title,eigen_centrality)
 final_2022 <- left_join(df_ec_2022,code_2022,by = c("id" = "code")) %>% select(id,title,eigen_centrality)
 
 head(final_2010)
+head(final_2015)
 head(final_2020)
 head(final_2022)
 
@@ -84,15 +101,17 @@ example <- createWorkbook("example")
 example
 
 addWorksheet(example, "eigen_2010")
+addWorksheet(example, "eigen_2015")
 addWorksheet(example, "eigen_2020")
 addWorksheet(example, "eigen_2022")
 example
 
 writeDataTable(example,"eigen_2010",final_2010)
+writeDataTable(example,"eigen_2015",final_2015)
 writeDataTable(example,"eigen_2020",final_2020)
 writeDataTable(example,"eigen_2022",final_2022)
 
-saveWorkbook(example, file="/Users/haley/Desktop/2025-1/Research/data/Korea/data_result/industry_result.xlsx")
+saveWorkbook(example, file="/Users/haley/Desktop/2025-1/Research/data/Korea/data_result/industry_result_final.xlsx")
 
 
 ## For Gephi Visualization
@@ -104,6 +123,12 @@ data_2010_edge <-
   left_join(data_2010_fin,code_2010,by = c("from" = "code")) %>% 
   rename(from2=title) %>% 
   left_join(code_2010,by = c("to" = "code")) %>%
+  rename(to2=title) %>% select("from","to","from2","to2","weight") %>% rename(rom_label=from2, to_label=to2)
+
+data_2015_edge <- 
+  left_join(data_2015_fin,code_2015,by = c("from" = "code")) %>% 
+  rename(from2=title) %>% 
+  left_join(code_2015,by = c("to" = "code")) %>%
   rename(to2=title) %>% select("from","to","from2","to2","weight") %>% rename(rom_label=from2, to_label=to2)
 
 data_2020_edge <- 
@@ -119,22 +144,26 @@ data_2022_edge <-
   rename(to2=title) %>% select("from","to","from2","to2","weight") %>% rename(from_label=from2, to_label=to2)
 
 head(data_2010_edge)
+head(data_2015_edge)
 head(data_2020_edge)
 head(data_2022_edge)
 
 # save EDGE table
 write.csv(data_2010_edge, "/Users/haley/Desktop/2025-1/Research/data/Korea/edge_industry_2010", row.names = FALSE)
+write.csv(data_2015_edge, "/Users/haley/Desktop/2025-1/Research/data/Korea/edge_industry_2015", row.names = FALSE)
 write.csv(data_2020_edge, "/Users/haley/Desktop/2025-1/Research/data/Korea/edge_industry_2020", row.names = FALSE)
 write.csv(data_2022_edge, "/Users/haley/Desktop/2025-1/Research/data/Korea/edge_industry_2022", row.names = FALSE)
 
 
 # make NODe csv file
 data_2010_node <-final_2010 %>% rename(label=title) %>% select(id, label, eigen_centrality)
-data_2020_node <-final_2010 %>% rename(label=title) %>% select(id, label, eigen_centrality)
-data_2022_node <-final_2010 %>% rename(label=title) %>% select(id, label, eigen_centrality)
+data_2015_node <-final_2015 %>% rename(label=title) %>% select(id, label, eigen_centrality)
+data_2020_node <-final_2020 %>% rename(label=title) %>% select(id, label, eigen_centrality)
+data_2022_node <-final_2022 %>% rename(label=title) %>% select(id, label, eigen_centrality)
 
 # save NODE table
 write.csv(data_2010_node, "/Users/haley/Desktop/2025-1/Research/data/Korea/node_industry_2010", row.names = FALSE)
+write.csv(data_2015_node, "/Users/haley/Desktop/2025-1/Research/data/Korea/node_industry_2015", row.names = FALSE)
 write.csv(data_2020_node, "/Users/haley/Desktop/2025-1/Research/data/Korea/node_industry_2020", row.names = FALSE)
 write.csv(data_2022_node, "/Users/haley/Desktop/2025-1/Research/data/Korea/node_industry_2022", row.names = FALSE)
 
